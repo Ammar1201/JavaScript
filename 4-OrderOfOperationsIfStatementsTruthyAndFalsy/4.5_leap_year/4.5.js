870078bd83efd3ff9318deaff8a2390624a0a7c6
Ammar1201 <aamour1201@gmail.com>
2022-09-19 23:34:48 +0300

    edit

--- 4-OrderOfOperationsIfStatementsTruthyAndFalsy/4.5_leap_year/4.5.js
+++ 4-OrderOfOperationsIfStatementsTruthyAndFalsy/4.5_leap_year/4.5.js
@@ -1,7 +1,7 @@
 
 const leapYear = (year) => {
 
-  if ( year % 4==0 && year % 100 != 0 || year % 400 ==0)
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
     console.log(`it is indeed a lep year`);
   else
     console.log(`This is not a leap year`);
