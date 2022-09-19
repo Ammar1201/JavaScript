d74c57e0c20c29ae193abcf731ea567075015e13
Ammar1201 <aamour1201@gmail.com>
2022-09-19 21:55:20 +0300

    3.3 update

--- 3-DifferentWaysToDeclareFunctions/3.3_function_declarations_vs_expressions/3.3.js
+++ 3-DifferentWaysToDeclareFunctions/3.3_function_declarations_vs_expressions/3.3.js
@@ -2,4 +2,17 @@ function precentageOfWorld1 (population) {
   return (population / 7900) * 100;
 }
 
+const precentageOfWorld2 = (population) => {
+  return (population / 7900) * 100;
+}
+
 console.log(precentageOfWorld1(1441).toFixed(1));
+console.log(precentageOfWorld1(1234).toFixed(1));
+console.log(precentageOfWorld1(1861).toFixed(1));
+
+console.log(`------------------------`);
+
+console.log(precentageOfWorld2(1441).toFixed(1));
+console.log(precentageOfWorld2(1234).toFixed(1));
+console.log(precentageOfWorld2(1861).toFixed(1));
+
