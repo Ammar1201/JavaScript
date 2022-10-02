8cdff8e2eda3f2834523df8361a1c4b2d0f315d4
Ammar1201 <aamour1201@gmail.com>
2022-10-02 17:05:52 +0300

    exercise 12.3 done

--- 12-findAndFilter/12.3_school_method/12.3.js
+++ 12-findAndFilter/12.3_school_method/12.3.js
@@ -1,8 +1,9 @@
+const { LOG10E } = require("mathjs");
 
 const school = {
   teachers: [
-  { id: 1, name: "Pinchas", subjects: ["chemistry", "biology", "physics"], students: [], capacityLeft: 3 },
-  { id: 2, name: "Williams", subjects: ["history", "ethics"], students: [], capacityLeft: 2 },
+    { id: 1, name: "Pinchas", subjects: ["chemistry", "biology", "physics"], students: [], capacityLeft: 3 },
+    { id: 2, name: "Williams", subjects: ["history", "ethics"], students: [], capacityLeft: 2 },
   ],
   students: [
   { id: 10, name: "Jennifer", age: 20 },
@@ -11,31 +12,63 @@ const school = {
   { id:13, name: "Houston", age: 21 },
   ],
 
-  test: `hello`,
-
   findPerson: (type, id) => {
     return type.find((person) => {
       return person.id == id;
     });
   },
 
+  findTeacherBySubject: (subject) => {
+    return school.teachers.find((teacher) => {
+      return teacher.subjects.includes(subject) && teacher.capacityLeft > 0;
+    });
+  },
+
   assignStudent: (students, subject) => {
-    // let teacher = findSubject(this.teachers, subject);
-    let teacher = this[this.teachers];
-    console.log(teacher);
+    let teacher = school.findTeacherBySubject(subject);
+    let teacherCapacity = teacher.capacityLeft;
+    let breakLoop = false;
+    while(teacherCapacity > 0 && !breakLoop) {
+      while(teacherCapacity > 0 && !breakLoop) {
+        let student = null;
+        if(students.length > 0) {
+          student = students.pop();
+        }
+        else {
+          breakLoop = true;
+        }
+        if(!breakLoop) {
+          teacher.students.push(student);
+          teacher.capacityLeft -= 1;
+          teacherCapacity -= 1;
+        }
+      }
+      if(!breakLoop && teacherCapacity > 0) {
+        teacher = school.findTeacherBySubject(subject);
+        teacherCapacity = teacher.capacityLeft;
+      }
+    }
+
+    if(students.length > 0) {
+      console.log(`Sorry, no available teachers left`);
+    }
+    else {
+      console.log(`done`);
+    }
+  },
+
+  assignTeachersSubject: (teacherID, subject) => {
+    let teacher = school.findPerson(school.teachers, teacherID);
+    if(!(teacher.subjects.includes(subject))) {
+      teacher.subjects.push(subject);
+    }
   }
 };
 
-function findSubject(type, subject) {
-  return type.find((person) => {
-    return person.name == subject;
-  });
-}
-
-let tmp = findSubject(school.teachers, `Pinchas`);
-
-// console.log(tmp.subjects.includes(`biology`));
-
 // console.log(school.findPerson(school.students, 12));
 
-school.assignStudent(school.teachers, `biology`);
+// school.assignStudent([11, 12, 10], `biology`);
+// console.log(school.teachers);
+
+school.assignTeachersSubject(2, `ethics`);
+console.log(school.teachers);
