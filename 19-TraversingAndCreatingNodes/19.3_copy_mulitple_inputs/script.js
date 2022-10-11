9d1f38eb67b24552d514a72fb2c8439bc4014cc4
Ammar1201 <aamour1201@gmail.com>
2022-10-11 21:33:56 +0300

    19.3 auto-submit & auto-paste done After each value is inserted, the next input will be focused

--- 19-TraversingAndCreatingNodes/19.3_copy_mulitple_inputs/script.js
+++ 19-TraversingAndCreatingNodes/19.3_copy_mulitple_inputs/script.js
@@ -1,18 +1,39 @@
 
 const inputs = document.querySelectorAll('.input');
-// console.log(inputs);
 
-// const copiedData = ClipboardEvent.clipboardData();
+// const verficationCode = document.querySelectorAll('label')[1];
 
-// const copiedData = new DataTransfer(ClipboardEvent.clipboardData);
-// console.log(copiedData);
+// verficationCode.addEventListener('copy', (event) => {
+//   console.log(event);
+// });
 
-const verficationCode = document.querySelectorAll('label')[1];
-// console.log(label);
-verficationCode.addEventListener('copy', (event) => {
-  
+document.querySelector('#form').addEventListener("submit", function(event) {
+  event.preventDefault()
 });
 
-verficationCode.addEventListener('paste', (event) => {
+document.querySelector('#form').addEventListener('paste', pasting);
 
-});
+function pasting(event) {
+  let text = event.clipboardData.getData('text/plain');
+  let counter = 0;
+
+  if(text.length > 6) {
+    return;
+  }
+
+  for(let i = 0; i < text.length; i++) {
+    if(text[i] === undefined) {
+      inputs[i].value == '';
+    }
+    else {
+      inputs[i].value = text[i];
+      counter += 1;
+    }
+  }
+
+  event.preventDefault();
+
+  if(counter === 6) {
+    document.getElementById('form').submit();
+  }
+}
