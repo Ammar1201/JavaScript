4ce6004052de2711b8762349ed3b1c0c0c25fe82
Ammar1201 <aamour1201@gmail.com>
2022-10-11 17:01:41 +0300

    fix 19.1

--- 19-TraversingAndCreatingNodes/19.1_dom_walk/script.js
+++ 19-TraversingAndCreatingNodes/19.1_dom_walk/script.js
@@ -1,19 +1,36 @@
 
 const startHere = document.querySelector('.start-here');
 
+//* 1. Change the text from “title 2” to “main title”
 startHere.textContent = 'main title';
 
+
+//* 2. Add another subtitle with the text “subtitle 4”
+// const subtitle4 = document.createElement('li');
+// subtitle4.textContent = 'subtitle 4';
+// document.querySelectorAll('ul')[1].appendChild(subtitle4);
+
+const ul = startHere.nextElementSibling.firstElementChild;
 const subtitle4 = document.createElement('li');
 subtitle4.textContent = 'subtitle 4';
-document.querySelectorAll('ul')[1].appendChild(subtitle4);
+ul.appendChild(subtitle4);
 
 
-document.querySelector('title').text = 'Master Of The Dom';
+//* 3. Delete the last <li> element from the list.
+// const lastLI = document.querySelectorAll('li')[7];
+// lastLI.remove();
 
-document.querySelector('div p').textContent = 'The DOM is annoying';
+startHere.parentElement.lastElementChild.remove();
 
 
-const lastLI = document.querySelectorAll('li')[7];
-lastLI.remove();
+//* 4. Change the <title> element text to “Master Of The Dom”.
+// document.querySelector('title').text = 'Master Of The Dom';
 
-// document.querySelector('ul').removeChild(document.querySelector('ul')[3]);
+// document.head.lastElementChild.textContent = 'Master Of The Dom';
+
+startHere.parentElement.parentElement.parentElement.firstChild.lastElementChild.textContent = 'Master Of The Dom';
+
+//* 5. Change the text of the <p> element ot something else of your
+// document.querySelector('div p').textContent = 'The DOM is annoying';
+
+startHere.parentElement.nextElementSibling.firstElementChild.textContent = 'Traverse is annoying';
---
+++ 19-TraversingAndCreatingNodes/19.3_copy_mulitple_inputs/script.js
@@ -0,0 +1,18 @@
+
+const inputs = document.querySelectorAll('.input');
+// console.log(inputs);
+
+// const copiedData = ClipboardEvent.clipboardData();
+
+// const copiedData = new DataTransfer(ClipboardEvent.clipboardData);
+// console.log(copiedData);
+
+const verficationCode = document.querySelectorAll('label')[1];
+// console.log(label);
+verficationCode.addEventListener('copy', (event) => {
+  
+});
+
+verficationCode.addEventListener('paste', (event) => {
+
+});
