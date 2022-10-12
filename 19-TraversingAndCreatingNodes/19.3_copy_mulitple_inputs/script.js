9090d59763ea41dd806b863c6cf3f46dd575dac7
Ammar1201 <aamour1201@gmail.com>
2022-10-12 10:56:16 +0300

    fixed 19.3 after solving with mordi (old code still there)

--- 19-TraversingAndCreatingNodes/19.3_copy_mulitple_inputs/script.js
+++ 19-TraversingAndCreatingNodes/19.3_copy_mulitple_inputs/script.js
@@ -1,53 +1,86 @@
 
-const inputs = document.querySelectorAll('.input');
+const inputs = document.querySelectorAll('input[type="text"]');
+
+
+const handleInput = (event) => {
+  const input = event.target;
+
+  if(input.length > 0 && !Number(input.value)) {
+    return;
+  }
+
+  if(input.nextElementSibling) {
+    input.nextElementSibling.focus();
+  }
+
+  const allValuesFull = [...inputs].every((input) => input.value);
+  if(allValuesFull) {
+    document.querySelector('form').submit();
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', handleInput);
+});
+
+const handlePaste = event => {
+  const paste = event.clipboardData.getData('text/plain');
+  inputs.forEach((input, index) => {
+    input.value = paste[index];
+  });
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('paste', handlePaste);
+});
 
 // document.querySelector('#form').addEventListener("submit", function(event) {
 //   event.preventDefault()
 // });
 
-document.querySelector('#form').addEventListener('paste', pasting);
+// document.querySelector('#form').addEventListener('paste', pasting);
 
-function pasting(event) {
-  let text = event.clipboardData.getData('text/plain');
-  let counter = 0;
+// function pasting(event) {
+//   let text = event.clipboardData.getData('text/plain');
+//   let counter = 0;
 
-  if(text.length > 6) {
-    return;
-  }
+//   if(text.length > 6) {
+//     return;
+//   }
 
-  for(let i = 0; i < text.length; i++) {
-    if(text[i] === undefined) {
-      inputs[i].value == '';
-    }
-    else {
-      inputs[i].value = text[i];
-      counter += 1;
-    }
-  }
+//   for(let i = 0; i < text.length; i++) {
+//     if(text[i] === undefined) {
+//       inputs[i].value == '';
+//     }
+//     else {
+//       inputs[i].value = text[i];
+//       counter += 1;
+//     }
+//   }
 
-  event.preventDefault();
+//   event.preventDefault();
 
-  if(counter === 6) {
-    document.getElementById('form').submit();
-  }
-}
+//   if(counter === 6) {
+//     document.getElementById('form').submit();
+//   }
+// }
 
-document.querySelector('#form').addEventListener('keyup', moveNext);
+// document.querySelector('#form').addEventListener('keyup', moveNext);
 
-function moveNext(event) {
-  let target = event.srcElement || e.target;
-  let maxLength = parseInt(target.attributes["maxlength"].value, 10);
-  let myLength = target.value.length;
-  if (myLength >= maxLength) {
-    let next = target;
-    while (next = next.nextElementSibling) {
-      if (next == null) {
-          break;
-      }
-      if (next.tagName.toLowerCase() === "input") {
-        next.focus();
-        break;
-      }
-    }
-  }
-}
+// function moveNext(event) {
+//   let target = event.srcElement || e.target;
+//   let maxLength = parseInt(target.attributes["maxlength"].value, 10);
+//   let myLength = target.value.length;
+//   if (myLength >= maxLength) {
+//     let next = target;
+//     while (next = next.nextElementSibling) {
+//       if (next == null) {
+//           break;
+//       }
+//       if (next.tagName.toLowerCase() === "input") {
+//         next.focus();
+//         break;
+//       }
+//     }
+//   }
+// }
