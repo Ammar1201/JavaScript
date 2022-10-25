020004e854b6ed07923f6c829cade743443163cb
Ammar1201 <aamour1201@gmail.com>
2022-10-25 13:03:02 +0300

    exercise  28.1 simple fix

--- 28-Ajax&MakingHTTPrequests/28.1_simple_fetch/script.js
+++ 28-Ajax&MakingHTTPrequests/28.1_simple_fetch/script.js
@@ -9,13 +9,13 @@ jokeBtn.addEventListener('click', (event) => {
         throw new Error('Error fetching');
       }
       else {
-        res.json().then((data) => {
-          console.log(data);
-          const label = document.createElement('label');
-          label.innerHTML = `<br> Joke Title is → ${data.contents.jokes[0].joke.title}<br> The joke is → ${data.contents.jokes[0].joke.text}`;
-          document.body.appendChild(label);
-        })
+        return res.json()
       }
+    })
+    .then((data) => {
+      const label = document.createElement('label');
+      label.innerHTML = `<br> Joke Title is → ${data.contents.jokes[0].joke.title}<br> The joke is → ${data.contents.jokes[0].joke.text}`;
+      document.body.appendChild(label);
     })
     .catch((err) => {
       console.log(err);
