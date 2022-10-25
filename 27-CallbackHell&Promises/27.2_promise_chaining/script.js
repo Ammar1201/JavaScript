83c3e13480ee76f8217978717e1e67a2963ca3cf
Ammar1201 <aamour1201@gmail.com>
2022-10-25 15:17:39 +0300

    exercise 27.2 simple fix

--- 27-CallbackHell&Promises/27.2_promise_chaining/script.js
+++ 27-CallbackHell&Promises/27.2_promise_chaining/script.js
@@ -1,48 +1,4 @@
 
-
-//! I have added 2 versions for the question because I didn't fully understand the question
-//! I think the second version is the correct one
-
-// function makeAllCaps(words) {
-//   return new Promise((resolved, rejected) => {
-//     const capitals = words.map(word => {
-//       if(typeof word === 'string') {
-//         return word.toUpperCase();
-//       }
-//       else {
-//         rejected();
-//       }
-//     });
-
-//     function sortWords(){
-//       resolved(capitals.sort());
-//     }
-
-//     sortWords();
-//   });
-// }
-
-// const str = ['hello', 'words', 'world', 'hidden', 'hi'];
-
-// const arr = ['hello', 'words', 'world', 'hidden', 'hi', 25];
-
-// makeAllCaps(str)
-//   .then((data) => {
-//     console.log('your words capitalized and sorted', data);
-//   })
-//   .catch(() => {
-//     console.log('your words include something other than a string', str);
-//   });
-
-// makeAllCaps(arr)
-//   .then((data) => {
-//     console.log('your words capitalized and sorted', data);
-//   })
-//   .catch(() => {
-//     console.log('your words include something other than a string', arr);
-//   });
-
-
 function makeAllCaps(words) {
   return new Promise((resolved, rejected) => {
     const capitals = words.map(word => {
