1223bc8a4510ba4e89953b7d3948c0e122d93242
Ammar1201 <aamour1201@gmail.com>
2022-10-25 10:42:41 +0300

    27.2 added 2 versions

--- 27-CallbackHell&Promises/27.2_promise_chaining/script.js
+++ 27-CallbackHell&Promises/27.2_promise_chaining/script.js
@@ -10,12 +10,18 @@ function makeAllCaps(words) {
       }
     });
 
-    function sortWords() {
-      capitals.sort();
-      resolved(capitals);
-    }
+    resolved(capitals);
+  });
+}
 
-    sortWords();
+function sortWords(words) {
+  return new Promise((resolved, rejected) => {
+    words.forEach(word => {
+      if(typeof word !== 'string') {
+        rejected();
+      }
+    });
+    resolved(words.sort());
   });
 }
 
@@ -24,17 +30,88 @@ const str = ['hello', 'words', 'world', 'hidden', 'hi'];
 const arr = ['hello', 'words', 'world', 'hidden', 'hi', 25];
 
 makeAllCaps(str)
-  .then((data) => {
-    console.log('your words capitalized and sorted', data);
+  .then((capitalized) => {
+    sortWords(capitalized)
+    .then((sorted) => {
+      console.log('your words capitalized and sorted', sorted);
+    })
+    .catch(() => {
+      console.log('your words include something other than a string', capitalized);
+    })
   })
   .catch(() => {
     console.log('your words include something other than a string', str);
   });
 
 makeAllCaps(arr)
-  .then((data) => {
-    console.log('your words capitalized and sorted', data);
+  .then((capitalized) => {
+    sortWords(capitalized)
+    .then((sorted) => {
+      console.log('your words capitalized and sorted', sorted);
+    })
+    .catch(() => {
+      console.log('your words include something other than a string', capitalized);
+    })
   })
   .catch(() => {
     console.log('your words include something other than a string', arr);
   });
+
+
+// function makeAllCaps(words) {
+//   return new Promise((resolved, rejected) => {
+//     const capitals = words.map(word => {
+//       if(typeof word === 'string') {
+//         return word.toUpperCase();
+//       }
+//       else {
+//         rejected();
+//       }
+//     });
+
+//     resolved(capitals);
+//   });
+// }
+  
+// function sortWords(words) {
+//   return new Promise((resolved, rejected) => {
+//     words.forEach(word => {
+//       if(typeof word !== 'string') {
+//         rejected();
+//       }
+//     });
+//     resolved(words.sort());
+//   });
+// }
+  
+// const str = ['hello', 'words', 'world', 'hidden', 'hi'];
+
+// const arr = ['hello', 'words', 'world', 'hidden', 'hi', 25];
+
+// makeAllCaps(str)
+//   .then((capitalized) => {
+//     sortWords(capitalized)
+//     .then((sorted) => {
+//       console.log('your words capitalized and sorted', sorted);
+//     })
+//     .catch(() => {
+//       console.log('your words include something other than a string', capitalized);
+//     })
+//   })
+//   .catch(() => {
+//     console.log('your words include something other than a string', str);
+//   });
+
+// makeAllCaps(arr)
+//   .then((capitalized) => {
+//     sortWords(capitalized)
+//     .then((sorted) => {
+//       console.log('your words capitalized and sorted', sorted);
+//     })
+//     .catch(() => {
+//       console.log('your words include something other than a string', capitalized);
+//     })
+//   })
+//   .catch(() => {
+//     console.log('your words include something other than a string', arr);
+//   });
