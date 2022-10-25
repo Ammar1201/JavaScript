6570d553e0ee794b603bc5363982400b7d177039
Ammar1201 <aamour1201@gmail.com>
2022-10-25 15:56:51 +0300

    added 29-Async&Await exercises and solved

---
+++ 29-Async&Await/29.2_refactor_with_async_await/script.js
@@ -0,0 +1,39 @@
+
+const jokeBtn = document.querySelector('button');
+
+// jokeBtn.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   fetch('https://api.jokes.one/jod')
+//     .then((res) => {
+//       if(!res.ok) {
+//         throw new Error('Error fetching');
+//       }
+//       else {
+//         return res.json()
+//       }
+//     })
+//     .then((data) => {
+//       const label = document.createElement('label');
+//       label.innerHTML = `<br> Joke Title is → ${data.contents.jokes[0].joke.title}<br> The joke is → ${data.contents.jokes[0].joke.text}`;
+//       document.body.appendChild(label);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+jokeBtn.addEventListener('click' , async function(){
+  try {
+    const res = await fetch('https://api.jokes.one/jod');
+    if(!res.ok) {
+      throw new Error('Error fetching');
+    }
+    const data = await res.json();
+    const label = document.createElement('label');
+    label.innerHTML = `<br> Joke Title is → ${data.contents.jokes[0].joke.title}<br> The joke is → ${data.contents.jokes[0].joke.text}`;
+    document.body.appendChild(label);
+  }
+  catch(error) {
+    console.log(error);
+  }
+});
