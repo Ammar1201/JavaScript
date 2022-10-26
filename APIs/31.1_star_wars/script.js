9480675747374df328a7f2680bf6b961ce84b9a9
Ammar1201 <aamour1201@gmail.com>
2022-10-26 21:28:33 +0300

    31.1_star_wars dynamic data diplayed done

--- APIs/31.1_star_wars/script.js
+++ APIs/31.1_star_wars/script.js
@@ -9,9 +9,34 @@
   5. Planet population
  */
 
-// const data = [];
-
 const table = document.createElement('table');
+
+const initializeTable = () => {
+  document.body.appendChild(table);
+  const th = document.createElement('th');
+  th.setAttribute('colspan', '5');
+  th.textContent = 'Star Wars';
+  table.appendChild(th);
+}
+
+const addDataKeys = (data) => {
+  const row = document.createElement('tr');
+  table.appendChild(row);
+  for(let key in data) {
+    if(typeof data[key] !== 'object') {
+      const th = document.createElement('th');
+      th.textContent = key;
+      row.appendChild(th);
+    }
+    else {
+      for(let value in data[key]) {
+        const th = document.createElement('th');
+        th.textContent = `planet ${value}`;
+        row.appendChild(th);
+      }
+    }
+  }
+};
 
 const fetchData = async (num) => {
   try {
@@ -47,13 +72,31 @@ const fetchNCharacters = async (num) => {
 const displayData = async () => {
   const data = await fetchNCharacters(10);
 
+  initializeTable();
+  addDataKeys(data[0]);
 
-
+  for(let i = 0; i < data.length; i++) {
+    const row = document.createElement('tr');
+    for(let key in data[i]) {
+      if(typeof data[i][key] !== 'object') {
+        const td = document.createElement('td');
+        td.textContent = data[i][key];
+        row.appendChild(td);
+      }
+      else {
+        for(let objKey in data[i][key]) {
+          const td = document.createElement('td');
+          td.textContent = `${data[i][key][objKey]}`;
+          row.appendChild(td);
+        }
+      }
+    }
+    table.appendChild(row);
+  }
 };
 
 const start = async () => {
-  console.log(data);
-  displayData(data);
+  displayData();
 };
 
-// start();
+start();
