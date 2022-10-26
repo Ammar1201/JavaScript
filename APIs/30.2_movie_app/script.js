ef9114a74095bbf6e431d805016c45e7f6a14e9b
Ammar1201 <aamour1201@gmail.com>
2022-10-26 14:40:34 +0300

    30.2_movie_app display movie picture and other information

--- APIs/30.2_movie_app/script.js
+++ APIs/30.2_movie_app/script.js
@@ -3,9 +3,24 @@
 
 //* https://www.omdbapi.com/?apikey=c469cd9c&
 
+/**
+1. Movie poster **
+2. Movie title ***
+3. Genre **
+4. Year **
+5. Plot ** 
+6. Director **
+7. Actors 
+8. Ratings of movies from IMDB, rotten tomatoes, and
+Metacritic. (make sure that particular movie gives you all
+the ratings. It could be, that movie won’t give you all 3
+ratings)
+ */
+
 const searchInput = document.querySelector('input');
 const searchBtn = document.querySelector('button');
 const errorMsg = document.querySelector('#errorMsg');
+const movieContainer = document.querySelector('#movie-container');
 
 const fetchData = async (title) => {
   try {
@@ -17,6 +32,7 @@ const fetchData = async (title) => {
   
     const data = await res.json();
     console.log(data);
+    displayMovie(data);
   }
   catch(error) {
     errorMsg.textContent = `movie "${title}" not found`;
@@ -24,16 +40,48 @@ const fetchData = async (title) => {
 }
 
 const displayMovie = (movie) => {
+  const poster = document.createElement('img');
+  console.log(movie.Poster);
+  poster.src = movie.Poster;
+  movieContainer.appendChild(poster);
 
+  const information = document.createElement('div');
+  displayMovie.id = 'information';
+
+  movieContainer.appendChild(information);
+
+  const title = document.createElement('h1');
+  title.textContent = movie.Title;
+  information.appendChild(title);
+
+  const plot = document.createElement('p');
+  plot.textContent = movie.Plot;
+  information.appendChild(plot);
+
+  const director = document.createElement('h2');
+  director.textContent = `Director: ${movie.Director}`;
+  information.appendChild(director);
+
+  const genre = document.createElement('h2');
+  genre.textContent = `Genres: ${movie.Genre}`;
+  information.appendChild(genre);
+
+  const actors = document.createElement('h2');
+  actors.textContent = `Actors: ${movie.Actors}`;
+  information.appendChild(actors);
+
+  const released = document.createElement('h2');
+  released.textContent = `Released: ${movie.Released}`;
+  information.appendChild(released);
 };
 
 window.addEventListener('load', () => {
   searchInput.focus();
 })
 
-// searchInput.addEventListener('focus', () => {
-//   errorMsg.textContent = '';
-// });
+searchInput.addEventListener('focus', () => {
+  errorMsg.textContent = '';
+});
 
 searchBtn.addEventListener('click', () => {
   fetchData('lucy');
