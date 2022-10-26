ce274d8a3e2153e4f3c0d051660dd7eb0b741288
Ammar1201 <aamour1201@gmail.com>
2022-10-26 13:55:04 +0300

    30.2_movie_app basic styling and elements

--- APIs/30.2_movie_app/script.js
+++ APIs/30.2_movie_app/script.js
@@ -5,12 +5,36 @@
 
 const searchInput = document.querySelector('input');
 const searchBtn = document.querySelector('button');
-// const errorMsg = document.querySelector('#errorMsg');
+const errorMsg = document.querySelector('#errorMsg');
+
+const fetchData = async (title) => {
+  try {
+    const url = 'https://www.omdbapi.com/?apikey=c469cd9c&t=' + title;
+    const res = await fetch(url);
+    if(!res.ok) {
+      throw new Error('Error fetching');
+    }
+  
+    const data = await res.json();
+    console.log(data);
+  }
+  catch(error) {
+    errorMsg.textContent = `movie "${title}" not found`;
+  }
+}
+
+const displayMovie = (movie) => {
+
+};
 
 window.addEventListener('load', () => {
   searchInput.focus();
 })
 
-searchInput.addEventListener('focus', () => {
-  errorMsg.textContent = '';
+// searchInput.addEventListener('focus', () => {
+//   errorMsg.textContent = '';
+// });
+
+searchBtn.addEventListener('click', () => {
+  fetchData('lucy');
 });
