4461ae7eec28a7f4a01b66b61bb16b00c07bbc6f
Ammar1201 <aamour1201@gmail.com>
2022-10-26 21:57:17 +0300

    30.1_github_api small fixes and adding comments

--- APIs/30.1_github_api/script.js
+++ APIs/30.1_github_api/script.js
@@ -3,18 +3,9 @@ const searchInput = document.querySelector('input');
 const searchBtn = document.querySelector('button');
 const errorMsg = document.querySelector('#errorMsg');
 
-const userInformation = {
-  avatar_url: '',
-  login: '',
-  public_repos: 0,
-};
-
 const users = [];
 
-const isUserInformationEmpty = () => {
-  return userInformation.avatar_url === '' && userInformation.login === '' && userInformation.public_repos === 0;
-};
-
+//* Async - fetching Data
 const fetchData = async (user) => {
   if(users.includes(user)) {
     return;
@@ -28,41 +19,33 @@ const fetchData = async (user) => {
     }
   
     const data = await res.json();
-    userInformation.avatar_url = data.avatar_url;
-    userInformation.login = data.login;
-    userInformation.public_repos = data.public_repos;
+
+    const transformedData = {avatar_url: data.avatar_url, login: data.login, public_repos: data.public_repos};
+
     users.push(user);
-    appendToBody(data);
+    displayUser(transformedData);
   }
   catch(error) {
     errorMsg.textContent = `username "${user}" not found`;
   }
 }
 
-const appendToBody = async (user) => {
-
-  if(users.includes(user)) {
-    return;
-  }
-
-  if(isUserInformationEmpty()) {
-    return;
-  }
-
+//* displaying user information
+const displayUser = (user) => {
   const div = document.createElement('div');
   div.id = 'card';
   const avatar_img = document.createElement('img');
   const login = document.createElement('a');
   const repos = document.createElement('a');
 
-  avatar_img.src = userInformation.avatar_url;
+  avatar_img.src = user.avatar_url;
 
-  login.innerText = userInformation.login;
-  login.href = `https://github.com/${user}`;
+  login.innerText = user.login;
+  login.href = `https://github.com/${user.login}`;
   login.target = '_blank';
 
-  repos.innerText = userInformation.public_repos;
-  repos.href = `https://github.com/${user}?tab=repositories`;
+  repos.innerText = user.public_repos;
+  repos.href = `https://github.com/${user.login}?tab=repositories`;
   repos.target = '_blank';
 
   div.appendChild(avatar_img);
@@ -72,6 +55,7 @@ const appendToBody = async (user) => {
   document.body.append(div);
 }
 
+//* error handling
 const handleError = (user = '') => {
   if(user === '') {
     errorMsg.textContent = `*you must provide a username`;
@@ -80,52 +64,62 @@ const handleError = (user = '') => {
   errorMsg.textContent = `*user "${user}" is already displayed`;
 }
 
-window.addEventListener('load', () => {
-  searchInput.focus();
-})
+//*  Events ----------------------------
+const addEvents = () => {
+  window.addEventListener('load', () => {
+    searchInput.focus();
+  })
+  
+  searchInput.addEventListener('keydown', (event) => {
+    const key = event.key;
+    const user = event.target.value;
+  
+    if(key === 'Enter') {
+      if(user === '') {
+        handleError(user);
+        return;
+      }
+  
+      if(users.includes(user)){
+        handleError(user);
+        event.target.value = '';
+        return;
+      }
 
-searchInput.addEventListener('keydown', (event) => {
-  const key = event.key;
-  const user = event.target.value;
-
-  if(key === 'Enter') {
+      fetchData(user);
+      event.target.value = '';
+      searchInput.focus();
+    }
+  });
+  
+  searchBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const user = searchInput.value;
+  
     if(user === '') {
-      handleError(user);
+      handleError(user, 'Empty search');
       return;
     }
-
+  
     if(users.includes(user)){
       handleError(user);
-      event.target.value = '';
+      searchInput.value = '';
       return;
     }
+  
     fetchData(user);
-    event.target.value = '';
-    searchInput.focus();
-  }
-});
-
-searchBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  const user = searchInput.value;
-
-  if(user === '') {
-    handleError(user, 'Empty search');
-    return;
-  }
-
-  if(users.includes(user)){
-    handleError(user);
     searchInput.value = '';
-    return;
-  }
+    searchInput.focus();
+  });
+  
+  searchInput.addEventListener('focus', () => {
+    errorMsg.textContent = '';
+  });
+};
 
-  fetchData(user);
+//* main function - starting app
+const startApp = () => {
+  addEvents();
+};
 
-  searchInput.value = '';
-  searchInput.focus();
-});
-
-searchInput.addEventListener('focus', () => {
-  errorMsg.textContent = '';
-});
+startApp();
